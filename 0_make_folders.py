52c9155d91654e89e92762010acd3813cac07c04
lafasss <lafasss@gmail.com>
2023-11-29 14:44:11 +0200

    first update

---
+++ 0_make_folders.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 15:04:08 2023
+
+@author: lafas
+"""
+
+
+
+import os
+os.makedirs('unprocessed_audio_data', exist_ok=True)
+os.makedirs('processed_audio_data', exist_ok=True)
+
